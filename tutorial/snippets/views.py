c3b2dac2f74b272645a2d3c8a944cc49ad0ef2cf
Michael Stokley <michael@michaelstokley.com>
2016-05-11 11:41:47 -0700

    authentication permissions

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,13 +1,38 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
+from snippets.serializers import UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        """Associate user with snippet.
+
+        This method overrides the parent class method.
+        The create() method of serializer will receive an additional 'owner'
+        field along with validated data from request.
+        """
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
